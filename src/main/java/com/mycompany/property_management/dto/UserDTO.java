5c1bc4b38ba71b889164588f20128c274c647dd2
Sohrab Ali <callingsohrab@gmail.com>
2025-03-16 22:29:06 +0530

    Implementing logging using logback slf4j

--- src/main/java/com/mycompany/property_management/dto/UserDTO.java
+++ src/main/java/com/mycompany/property_management/dto/UserDTO.java
@@ -2,6 +2,9 @@ package com.mycompany.property_management.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +15,13 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+
+    @NotNull(message = "Owner email is Mandatory")
+    @Size(min=1, max =50, message = "Owner email should be between 1 to 50 character in length")
+    @NotEmpty(message = "Owner email cannot be empty")
     private String ownerEmail;
     private String phoneNumber;
+    @NotNull(message = "Password is Mandatory")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
--- src/main/java/com/mycompany/property_management/exception/BusinessExceptionHandler.java
+++ src/main/java/com/mycompany/property_management/exception/BusinessExceptionHandler.java
@@ -1,19 +1,49 @@
 package com.mycompany.property_management.exception;
 
-import ch.qos.logback.core.model.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
 public class BusinessExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(BusinessExceptionHandler.class);
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException bex) {
-        System.out.println("Business Exception is thrown : " + bex.getMessage());
+        for (ErrorModel em : bex.getErrors()) {
+            logger.info("Inside field validation  level - info : {} - {} ",em.getCode(), em.getMessage());
+            logger.debug("Inside field validation level - debug: {} - {} ",em.getCode(), em.getMessage());
+            logger.trace("Inside field validation level - trace : {} - {} ",em.getCode(), em.getMessage());
+            logger.warn("Inside field validation level - warn : {} - {} ",em.getCode(), em.getMessage());
+            logger.error("Inside field validation level - error : {} - {} ",em.getCode(), em.getMessage());
+
+        }
         return new ResponseEntity<List<ErrorModel>>(bex.getErrors(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorModel>> handleFieldValidation(MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        List<ErrorModel> errorModels = new ArrayList<>();
+
+        for (FieldError fieldError : fieldErrors) {
+            logger.info("Inside field validation : {} - {} ",fieldError.getField(),fieldError.getDefaultMessage());
+            logger.debug("Inside field validation : {} - {} ",fieldError.getField(),fieldError.getDefaultMessage());
+            ErrorModel errorModel = new ErrorModel();
+            errorModel.setCode(fieldError.getField());
+            errorModel.setMessage(fieldError.getDefaultMessage());
+            errorModels.add(errorModel);
+        }
+        return new ResponseEntity<List<ErrorModel>>(errorModels, HttpStatus.BAD_REQUEST);
     }
 }
